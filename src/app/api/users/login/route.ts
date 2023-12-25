94c88a0f8cf6efd7e73558ddfb48abc2f2c15eef
Sarfraz Alam <sarfrazk7858@gmail.com>
2023-12-25 12:08:36 +0530

    login

---
+++ src/app/api/users/login/route.ts
@@ -0,0 +1,38 @@
+import { connect } from "@/dbConfig/dbConfig";
+import User from "@/models/userModel";
+import { NextRequest, NextResponse } from "next/server";
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+
+connect()
+
+export default async function POST(req: NextRequest) {
+    try {
+        const reqBody = await req.json()
+        const { email, password } = reqBody
+
+        const user = await User.findOne({ email })
+        if (!user) {
+            return NextResponse.json({ error: "User does not exists" }, { status: 400 })
+        }
+
+        const validedPassword = await bcrypt.compare(password, user.password)
+
+        const tokenData = {
+            id: user._id,
+            username: user.username,
+            email: user.email
+        }
+
+        const token = jwt.sign(tokenData, process.env.JWT_SECRET_KEY!, { expiresIn: "1h" })
+
+        const response = NextResponse.json({ message: "Login successful", success: true },)
+
+        response.cookies.set("token", token, { httpOnly: true })
+
+        return response;
+
+    } catch (error: any) {
+        return NextResponse.json({ error: error.message }, { status: 500 })
+    }
+}
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,15 +1,36 @@
 "use client";
 import { useRouter } from "next/navigation";
 import axios from "axios";
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
+import toast from "react-hot-toast";
 
 export default function LoginPage() {
-  const [user, setUser] = React.useState({
-    email: "",
-    password: "",
-  });
-  const onLogin = async () => {};
+  const router = useRouter();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const onLogin = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.post("/api/users/login", {
+        email,
+        password,
+      });
+      console.log("login success", response.data);
+      toast.success("Login success");
+      router.push("/profile");
+      
+    } catch (error: any) {
+      console.log("Login failed ", error.message);
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1>Login now</h1>
@@ -19,16 +40,16 @@ export default function LoginPage() {
       <input
         className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
         type="text"
-        value={user.email}
-        onChange={(e) => setUser({ ...user, email: e.target.value })}
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
         placeholder="email"
       />
       <label htmlFor="password">Password</label>
       <input
         className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
         type="text"
-        value={user.password}
-        onChange={(e) => setUser({ ...user, password: e.target.value })}
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
         placeholder="username"
       />
       <button
