05f58ca56eff3e85151aeccf459ebcc1c72236bb
Sarfraz Alam <sarfrazk7858@gmail.com>
2023-12-26 07:24:11 +0530

    get data

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,9 +2,12 @@
 import axios from "axios";
 import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
+import Link from "next/link";
 
 export default function ProfilePage() {
   const router = useRouter();
+  const [data, setData] = useState("nothing");
 
   const logout = async () => {
     try {
@@ -17,16 +20,35 @@ export default function ProfilePage() {
     }
   };
 
+  const getUserDetails = async () => {
+    const res = await axios.get("/api/users/me");
+    setData(res.data.data._id)
+    console.log(res.data.data._id);
+  };
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1>Profile</h1>
       <hr />
       <p>Profile page</p>
+      <h2 className="p-3 rounded bg-green-500 ">
+        {data === "nothing" ? (
+          "nothing"
+        ) : (
+          <Link href={`/profile/${data}`}>{data}</Link>
+        )}
+      </h2>
       <button
         onClick={logout}
         className="border py-2 px-12 bg-blue-500 text-white rounded"
       >
         Logout
+      </button>
+      <button
+        onClick={getUserDetails}
+        className="border py-2 px-12 bg-purple-600  text-white rounded"
+      >
+        Get User Data
       </button>
     </div>
   );
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,28 @@
-import  {NextResponse}  from "next/server";
-import type { NextRequest} from "next/server";
+import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
 
 
-export function middlerware(request:NextRequest){
-    return NextResponse.redirect(new URL('/home', request.url))
+export default function middlerware(request: NextRequest) {
+    const path = request.nextUrl.pathname
+
+    const isPublicPath = path === '/login' || path === '/signup'
+
+    const token = request.cookies.get('token')?.value || ""
+
+    if (isPublicPath && token) {
+        return NextResponse.redirect(new URL('/', request.nextUrl))
+    }
+    if (!isPublicPath && !token) {
+        return NextResponse.redirect(new URL('/login', request.nextUrl))
+    }
 }
 
+
 export const config = {
-    matcher:"/about/:path*",
+    matcher: [
+        '/',
+        '/profile',
+        '/login',
+        '/ignup'
+    ],
 }
